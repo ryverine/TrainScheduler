352d555d07ed7b9cc5852e3edee02ecfccbe7b3d
ryverine <rwcheek@gmail.com>
2019-04-24 17:36:26 -0400

    Adding google authentication. Allowing user to clcik row to update train data. Validation for input.

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,15 +29,23 @@ allow them to change a train's Name, Destination and Arrival Time (and then, by 
 
 As a final challenge, make it so that only users who log into the site with their Google or GitHub accounts can use your site. 
 You'll need to read up on Firebase authentication for this bonus exercise.
-
+// https://firebase.google.com/docs/auth/web/google-signin
+// https://firebase.google.com/docs/auth/web/github-auth
+// https://howtofirebase.com/firebase-authentication-for-web-d58aad62cf6d
+// https://www.youtube.com/watch?v=9kRgVxULbag
 
 */
 
 // Create GUID / UUID in JavaScript?
 // https://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
 
+// Military Time Chart
+// http://militarytimechart.com/
+
+
  $(document).ready(function() 
  {
+
 	// Initialize Firebase
 	var config = {	apiKey: "AIzaSyCZt_ec1cn1ETdIsj7mEtNAfrcepQNT4Ng",
 					authDomain: "train-scheduler-232b7.firebaseapp.com",
@@ -57,6 +65,56 @@ You'll need to read up on Firebase authentication for this bonus exercise.
 
 	var trainTableData = $("#trainDataArea");
 
+
+
+	// user signs-in with google account
+	$("#btn-googleSignIn").on("click", function()
+	{
+		event.preventDefault();
+		console.log("SIGN IN CLICKED");
+
+		var provider = new firebase.auth.GoogleAuthProvider();
+		firebase.auth().useDeviceLanguage();
+
+		provider.addScope("profile");
+		provider.addScope("email");
+
+		
+		return firebase.auth().signInWithPopup(provider).catch(function(error){
+			console.log("Google sign-in error: " + "\n" +  error);
+		});
+
+	});
+		/*
+
+
+
+firebase.auth().signInWithRedirect(provider);
+
+
+firebase.auth().getRedirectResult().then(function(result) {
+  if (result.credential) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // ...
+  }
+  // The signed-in user info.
+  var user = result.user;
+}).catch(function(error) {
+  // Handle Errors here.
+  var errorCode = error.code;
+  var errorMessage = error.message;
+  // The email of the user's account used.
+  var email = error.email;
+  // The firebase.auth.AuthCredential type that was used.
+  var credential = error.credential;
+  // ...
+});
+		*/
+		
+
+
+
 	// DISPLAY INFO ON LAST TRAIN ADDED
 	// $("#lastTrainName").val().trim();
 	// $("#lastTrainNameDestination").val().trim();
@@ -64,61 +122,216 @@ You'll need to read up on Firebase authentication for this bonus exercise.
 	// $("#lastTrainFreqency").val().trim();
 
 	// SUBMIT BUTTON
-	var allow = false;
+	
     $("#btn-submit").on("click", function() 
     {
         // prevent form from submitting
         event.preventDefault();
-        
         console.log("SUBMIT BUTTON CLICKED");
+  		$("#submitErrorMessage").text("");
 
-        if(allow)
-        {
-			trainName_input = $("#trainNameInput").val().trim();
-			trainDestination_input = $("#trainDestinationInput").val().trim();
-			firstTrainTime_input = $("#trainInitalTimeInput").val().trim();
-			trainFrequency_input = $("#trainFreqencyInput").val().trim();
+		trainName_input = $("#trainNameInput").val().trim();
+		trainDestination_input = $("#trainDestinationInput").val().trim();
+		firstTrainTimeHH_input = $("#trainInitalTimeHHInput").val().trim();
+		firstTrainTimeMM_input = $("#trainInitalTimeMMInput").val().trim();
+		trainFrequency_input = $("#trainFreqencyInput").val().trim();
+
+
+		// is this a train that we already have in the database?
+
+		var inputError = validateInput(trainName_input, trainDestination_input , firstTrainTimeHH_input, firstTrainTimeMM_input, trainFrequency_input);
+
+		if(inputError === "")
+		{
+			var startTime = firstTrainTimeHH_input + ":" + firstTrainTimeMM_input;
 
 			// add new object to firebase 
 	        database.ref().push(
 	        {
 	            name: trainName_input,
-	            destinaton: trainDestination_input,
-	            start: firstTrainTime_input,
+	            destination: trainDestination_input,
+	            start: startTime,
 	            frequency: trainFrequency_input
 	        });
+
+			$("#trainNameInput").val("");
+			$("#trainDestinationInput").val("");
+			$("#trainInitalTimeHHInput").val("");
+			$("#trainInitalTimeMMInput").val("");
+			$("#trainFreqencyInput").val("");
 		}
+		else
+		{
+			$("#submitErrorMessage").text(inputError);
+		}
+		
     });
 
+ 	function validateInput(name, dest, hh, mm, freq)
+ 	{
+ 		if(name != "" && dest != "" && hh != "" && mm != "" && freq != "")
+ 		{
+ 			if(hh < 0 || hh > 23)
+ 			{
+ 				return "Hours must be between 0 and 23!";
+ 			}
+ 			else if(mm < 0 || mm > 59)
+ 			{
+ 				return "Minutes must be between 0 and 59!";
+ 			}
+ 			else if(freq < 1 || freq > 1440)
+ 			{
+ 				return "Frequency must be between 1 and 1440!";
+ 			}
+ 			else
+ 			{
+ 				return "";
+ 			}
+ 		}
+ 		else
+ 		{
+ 			return "You must provide all requested data before submiting!";
+ 		}
+ 	}
+
+	database.ref().on("child_added", function(childSnapshot) 
+	{
+		// var trainTableData = $("#trainDataArea");
+
+		// get data from snapshot
+		var name = childSnapshot.val().name;
+		var destination = childSnapshot.val().destination;
+		var frequency = childSnapshot.val().frequency;
+		var start = childSnapshot.val().start;
+
+		if(start.charAt(2) === ":")
+		{
+			var timeValue = start.split(":");
+			findNextArrival(timeValue[0], timeValue[1], frequency);
+		}
+
+		// create elements for new table row
+		var newTableRow = $("<tr>");
+		newTableRow.attr("class", "trainData");
+
+		var newTableRow_name = $("<td>");
+		var newTableRow_destination = $("<td>");
+		var newTableRow_frequency = $("<td>");
+		var newTableRow_arrival = $("<td>");
+		var newTableRow_minutes = $("<td>");
+
+		// put data into row
+		newTableRow_name.text(name);
+		newTableRow_destination.text(destination);
+		newTableRow_frequency.text(frequency);
+
+		newTableRow.append(newTableRow_name);
+		newTableRow.append(newTableRow_destination);
+		newTableRow.append(newTableRow_frequency);
+		newTableRow.append(newTableRow_arrival);
+		newTableRow.append(newTableRow_minutes);
 
 
-    database.ref().on("child_added", function(childSnapshot) 
-    {
-		// Log everything that's coming out of snapshot
-		console.log(childSnapshot.val().name);
-		console.log(childSnapshot.val().email);
-		console.log(childSnapshot.val().age);
-		console.log(childSnapshot.val().comment);
-		console.log(childSnapshot.val().joinDate);
+		// moment js
+		// find next arrival
+		// find min
 
-		// full list of items to the well
-		$("#full-member-list").append("<div class='well'><span class='member-name'> " +
-		childSnapshot.val().name +
-		" </span><span class='member-email'> " + childSnapshot.val().email +
-		" </span><span class='member-age'> " + childSnapshot.val().age +
-		" </span><span class='member-comment'> " + childSnapshot.val().comment +
-		" </span></div>");
 
-		// Handle the errors
-		}, function(errorObject) {
-		console.log("Errors handled: " + errorObject.code);
+		// add row to table
+		trainTableData.append(newTableRow);
+
+		//Add info to last train added area
+		$("#lastTrainName").text(name);
+		$("#lastTrainNameDestination").text(destination);
+		$("#lastTrainStart").text(start);
+		$("#lastTrainFreqency").text(frequency);
+
+
+	// Handle the errors
+	}, function(errorObject) {
+	console.log("Errors handled: " + errorObject.code);
 	});
 
 
+ 	function findNextArrival(trainStartHH, trainStartMM, trainFrequency)
+ 	{
+ 		// https://momentjs.com/docs/#/get-set/
+ 		// https://www.tutorialspoint.com/momentjs/momentjs_durations.htm
+
+ 		var now = moment();
+
+ 		var trainStartTime = moment();
+ 		trainStartTime.hour(trainStartHH);
+ 		trainStartTime.minute(trainStartMM);
+
+ 		var theDuration = moment.duration(Number.parseInt(trainFrequency));
+ 		
+
+ 		console.log(	"findNextArrival(" + trainStartHH + ", " +trainStartMM  + ")" + "\n" +
+ 						"trainStartTime: " + trainStartTime);
+
+ 	}
+
+ 	function findMinutes(nextArrival)
+ 	{
+
+ 	}
 
 
+    $(document).on("click", "tr.trainData", function() 
+    {
+    	console.log("trainData clicked!");
+
+    	var children = $(this).children();
+
+    	var selectedTrain_name = children[0].innerText;
+    	var selectedTrain_dest = children[1].innerText;
+    	var selectedTrain_freq = children[2].innerText;
+    	// var selectedTrain_next = children[3].innerText;
+    	// var selectedTrain_mins = children[4].innerText;
 
 
+    	for(var i = 0; i < children.length; i++)
+    	{
+    		console.log("children["+i+"].innerText: " + children[i].innerText);
+    	}
+
+    	// https://firebase.google.com/docs/reference/js/firebase.database.Reference
+
+    	// console.log("database.ref(): " + "\n" + database.ref());
+
+    	// https://firebase.google.com/docs/reference/js/firebase.database.DataSnapshot
+
+		// JSON.parse(localStorage.getItem('userFavorites'));
+
+
+		// trying to find child with matching name
+		// need to get that child's "first train time" value
+		// split that value in ":"
+		// put [0] into HH input
+		// put [1] into MM input
+		/*
+    	database.ref().once("value")
+  			.then(function(snapshot) {
+  				console.log("snapshot: " + snapshot.child("name").val());
+    			var key = snapshot.key; // null
+    			var childKey = snapshot.child(selectedTrain_name).key; // "ada"
+    			console.log("key: " + key + "\n" + "childKey: " + childKey)
+  			});
+		*/
+    });
+
+
+// https://firebase.google.com/docs/database/admin/retrieve-data
+// https://firebase.google.com/docs/reference/js/firebase.database.Reference
+// https://firebase.google.com/docs/reference/js/firebase.database.Reference#once
+// https://firebase.google.com/docs/reference/js/firebase.database.Reference#tojson
+// https://firebase.google.com/docs/reference/js/firebase.database.Reference#on
+// https://firebase.google.com/docs/reference/js/firebase.database.Reference#update
+// https://firebase.google.com/docs/reference/js/firebase.database.DataSnapshot
+// https://firebase.google.com/docs/reference/js/firebase.database.Query
+
+// https://www.youtube.com/watch?v=F6UWb9FNnj4
 
  /*
 
@@ -198,6 +411,17 @@ You'll need to read up on Firebase authentication for this bonus exercise.
 	// 5 ...to determine what day of the week and what week of the year this randomDate falls on.
 	console.log(convertedDate.format("DDD"));
 	console.log(convertedDate.format("dddd"));
+
+	*/
+
+
+
+
+
+	/*
+
+	
+
 
 	*/
  });
